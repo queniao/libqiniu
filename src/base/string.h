8ad7cd405158c2261a42f6f83bc00c336352632a
Liang Tao <amethyst.black@gmail.com>
2016-04-14 14:23:51 +0800

    Revert "添加 qn_str_join_strings() ，将多个输入字符串以给定定界符分隔、合并，返回一个新字符串对象。"
    This reverts commit fcebcb9e6a95ebec03c4055b10f587add698e293.

--- src/base/string.h
+++ src/base/string.h
@@ -10,32 +10,14 @@ extern "C"
 
 typedef struct _QN_STRING
 {
-    const char * str;
     qn_size size;
-    char data[0];
+    const char * str;
+    const char data[0];
 } qn_string;
-
-#define QN_STR_MAX_SIZE ((~ (qn_size)0L) - sizeof(qn_string) - 1)
 
 extern qn_string * qn_str_create(const char * str, qn_size size);
 extern qn_string * qn_str_duplicate(qn_string * src);
 extern void qn_str_destroy(qn_string * self);
-
-//== Function qn_str_join_strings()
-//== Parameters:
-//==    delimiter   A string as delimiter, can be en empty one("").
-//==    ...         A list of `const char * str` and `qn_size size` arguments,
-//==                the two must appear in pairs. End the list with a NULL value.
-//== Return:
-//==    A new string object.
-//== ERRNO:
-//==    ENOMEM      No enough memory for allocating buffer space.
-//==    EINVALID    No pairs of string arguments are passed.
-//==    EOVERFLOW   The size of the joined string would overflow the max string size.
-extern qn_string * qn_str_join_strings(const char * delimiter, ...);
-
-//extern qn_string * qn_str_sprintf(const char * restrict format, ...);
-//extern qn_string * qn_str_snprintf(char * restrict str, qn_size size,  const char * restrict format, ...);
 
 extern qn_bool qn_str_compare(const qn_string * restrict s1, const qn_string * restrict s2);
 
