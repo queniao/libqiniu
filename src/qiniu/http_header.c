2b9ce2e1e50e02703198a540a3d35172601f2ff3
LIANG Tao <amethyst.black@gmail.com>
2016-08-04 18:26:14 +0800

    将 HTTP Header 相关逻辑独立成模块。

---
+++ src/qiniu/http_header.c
@@ -0,0 +1,176 @@
+#include <strings.h>
+
+#include "qiniu/base/errors.h"
+#include "qiniu/http_header.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// ---- Definition of HTTP header ----
+
+typedef unsigned short qn_http_header_pos;
+
+typedef struct _QN_HTTP_HEADER
+{
+    qn_string * entries;
+    qn_http_header_pos cnt;
+    qn_http_header_pos cap;
+} qn_http_header;
+
+qn_http_header_ptr qn_http_hdr_create(void)
+{
+    qn_http_header_ptr new_hdr = calloc(1, sizeof(qn_http_header));
+    if (!new_hdr) {
+        qn_err_set_no_enough_memory();
+        return NULL;
+    } // if
+
+    new_hdr->entries = calloc(8, sizeof(qn_string));
+    if (!new_hdr->entries) {
+        free(new_hdr);
+        qn_err_set_no_enough_memory();
+        return NULL;
+    } // if
+
+    new_hdr->cnt = 0;
+    new_hdr->cap = 8;
+    return new_hdr;
+}
+
+void qn_http_hdr_destroy(qn_http_header_ptr hdr)
+{
+    if (hdr) {
+        qn_http_hdr_reset(hdr);
+        free(hdr);
+    } // if
+}
+
+void qn_http_hdr_reset(qn_http_header_ptr hdr)
+{
+    while (hdr->cnt > 0) qn_str_destroy(hdr->entries[--hdr->cnt]);
+}
+
+static qn_bool qn_http_hdr_augment(qn_http_header_ptr hdr)
+{
+    qn_http_header_pos new_cap = hdr->cap + (hdr->cap >> 1); // 1.5 times
+    qn_string * new_entries = calloc(new_cap, sizeof(qn_string));
+    if (!new_entries) {
+        qn_err_set_no_enough_memory();
+        return qn_false;
+    } // if
+
+    memcpy(new_entries, hdr->entries, sizeof(qn_string) * hdr->cnt);
+    free(hdr->entries);
+    hdr->entries = new_entries;
+    hdr->cap = new_cap;
+    return qn_true;
+}
+
+static qn_http_header_pos qn_http_hdr_bsearch(qn_http_header_ptr hdr, const char * key, qn_size key_size)
+{
+    qn_http_header_pos begin = 0;
+    qn_http_header_pos end = hdr->cnt;
+    qn_http_header_pos mid = 0;
+
+    while (begin < end) {
+        mid = begin + ((end - begin) / 2);
+        if (strncasecmp(hdr->entries[mid], key, key_size) < 0) {
+            begin = mid + 1;
+        } else {
+            end = mid;
+        } // if
+    } // while
+    return begin;
+}
+
+qn_bool qn_http_hdr_set_raw(qn_http_header_ptr hdr, const char * key, qn_size key_size, const char * val, qn_size val_size)
+{
+    qn_string new_val = NULL;
+    qn_http_header_pos pos = 0;
+
+    if (hdr->cnt == hdr->cap && !qn_http_hdr_augment(hdr)) return qn_false;
+
+    new_val = qn_str_sprintf("%*s: %*s", key_size, key, val_size, val);
+    if (!new_val) {
+        qn_err_set_no_enough_memory();
+        return qn_false;
+    }
+
+    pos = qn_http_hdr_bsearch(hdr, key, key_size);
+    if (pos < hdr->cnt) {
+        qn_str_destroy(hdr->entries[pos]);
+        hdr->entries[pos] = new_val;
+    } else {
+        memmove(&hdr->entries[pos+1], &hdr->entries[pos], sizeof(qn_string) * (hdr->cnt - pos));
+        hdr->entries[pos] = new_val;
+        hdr->cnt += 1;
+    } // if
+    return qn_true;
+}
+
+void qn_http_hdr_unset_raw(qn_http_header_ptr hdr, const char * key, qn_size key_size)
+{
+    qn_http_header_pos pos = qn_http_hdr_bsearch(hdr, key, key_size);
+    if (pos < hdr->cnt) {
+        qn_str_destroy(hdr->entries[pos]);
+        hdr->cnt -= 1;
+    } // if
+}
+
+// ---- Definition of HTTP header iterator ----
+
+typedef struct _QN_HTTP_HDR_ITERATOR
+{
+    qn_http_header_ptr hdr;
+    qn_http_header_pos pos;
+} qn_http_hdr_iterator;
+
+qn_http_hdr_iterator_ptr qn_http_hdr_itr_create(qn_http_header_ptr hdr)
+{
+    qn_http_hdr_iterator_ptr new_itr = calloc(1, sizeof(qn_http_hdr_iterator));
+    if (!new_itr) {
+        qn_err_set_no_enough_memory();
+        return NULL;
+    } // if
+
+    new_itr->hdr = hdr;
+    return new_itr;
+}
+
+void qn_http_hdr_itr_destroy(qn_http_hdr_iterator_ptr itr)
+{
+    if (itr) {
+        free(itr);
+    }
+}
+
+void qn_http_hdr_itr_rewind(qn_http_hdr_iterator_ptr itr)
+{
+    itr->pos = 0;
+}
+
+qn_bool qn_http_hdr_itr_next_pair_raw(qn_http_hdr_iterator_ptr itr, const char ** key, qn_size * key_size, const char ** val, qn_size * val_size)
+{
+    qn_string entry;
+
+    if (itr->pos == itr->hdr->cnt) return qn_false;
+
+    *key = qn_str_cstr(entry = itr->hdr->entries[itr->pos++]);
+    *key_size = strchr(*key, ':') - *key;
+
+    *val = *key + 2;
+    *val_size = qn_str_size(entry) - *key_size - 2;
+    return qn_true;
+}
+
+const qn_string qn_http_hdr_itr_next_entry(qn_http_hdr_iterator_ptr itr)
+{
+    return (itr->pos == itr->hdr->cnt) ? NULL : itr->hdr->entries[itr->pos++];
+}
+
+#ifdef __cplusplus
+}
+#endif
+
---
+++ src/qiniu/http_header.h
@@ -0,0 +1,50 @@
+#ifndef __QN_HTTP_HEADER_H__
+#define __QN_HTTP_HEADER_H__
+
+#include "qiniu/base/string.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// ---- Declaration of HTTP header ----
+
+struct _QN_HTTP_HEADER;
+typedef struct _QN_HTTP_HEADER * qn_http_header_ptr;
+
+extern qn_http_header_ptr qn_http_hdr_create(void);
+extern void qn_http_hdr_destroy(qn_http_header_ptr hdr);
+extern void qn_http_hdr_reset(qn_http_header_ptr hdr);
+
+extern qn_bool qn_http_hdr_set_raw(qn_http_header_ptr hdr, const char * key, qn_size key_size, const char * val, qn_size val_size);
+
+static inline qn_bool qn_http_hdr_set(qn_http_header_ptr hdr, const qn_string key, const qn_string val)
+{
+    return qn_http_hdr_set_raw(hdr, qn_str_cstr(key), qn_str_size(key), qn_str_cstr(val), qn_str_size(val));
+}
+
+extern void qn_http_hdr_unset_raw(qn_http_header_ptr hdr, const char * key, qn_size key_size);
+
+static inline void qn_http_hdr_unset(qn_http_header_ptr hdr, const qn_string key)
+{
+    return qn_http_hdr_unset_raw(hdr, qn_str_cstr(key), qn_str_size(key));
+}
+
+// ---- Declaration of HTTP header iterator ----
+
+struct _QN_HTTP_HDR_ITERATOR;
+typedef struct _QN_HTTP_HDR_ITERATOR * qn_http_hdr_iterator_ptr;
+
+extern qn_http_hdr_iterator_ptr qn_http_hdr_itr_create(qn_http_header_ptr hdr);
+extern void qn_http_hdr_itr_destroy(qn_http_hdr_iterator_ptr itr);
+extern void qn_http_hdr_itr_rewind(qn_http_hdr_iterator_ptr itr);
+extern qn_bool qn_http_hdr_itr_next_pair_raw(qn_http_hdr_iterator_ptr itr, const char ** key, qn_size * key_size, const char ** val, qn_size * val_size);
+extern const qn_string qn_http_hdr_itr_next_entry(qn_http_hdr_iterator_ptr itr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __QN_HTTP_HEADER_H__
+
