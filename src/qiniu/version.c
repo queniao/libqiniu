6163d4ac32fd405d2e06533c13a05c22f486913f
LIANG Tao <amethyst.black@gmail.com>
2016-10-27 15:53:29 +0800

    添加基本的版本函数。

---
+++ src/qiniu/version.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "qiniu/version.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+static const char * qn_ver_full_string = "libqiniu-0.1.0";
+
+QN_API const char * qn_ver_get_full_string(void)
+{
+    return qn_ver_full_string;
+}
+
+QN_API void qn_ver_get_numbers(qn_version_ptr restrict ver)
+{
+    memset(ver, 0, sizeof(qn_version));
+    sscanf(qn_ver_full_string, "libqiniu-%d.%d.%d", &ver->major, &ver->minor, &ver->patch);
+}
+
+#ifdef __cplusplus
+}
+#endif
