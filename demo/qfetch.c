528c661a79988aaf432da15ee7f23bfe270f68e2
LIANG Tao <amethyst.black@gmail.com>
2016-10-31 16:23:06 +0800

    修正 demo/qfetch.c 对新接口的调用。

--- demo/qfetch.c
+++ demo/qfetch.c
@@ -8,13 +8,15 @@ int main(int argc, char * argv[])
     qn_string src_url;
     qn_string bucket;
     qn_string key;
-    qn_string fetch_ret;
+    qn_string fetch_ret_str;
+    qn_json_object_ptr fetch_ret;
     qn_storage_ptr stor;
     qn_stor_auth auth;
     qn_http_hdr_iterator_ptr hdr_itr;
     qn_string hdr_ent;
 
     if (argc < 6) {
+        printf("Demo qfetch - Fetch a file from any other server as if it is uploaded.\n");
         printf("Usage: qfetch <ACCESS_KEY> <SECRET_KEY> <SRC_URL> <BUCKET> <KEY>\n");
         return 0;
     } // if
@@ -26,6 +28,7 @@ int main(int argc, char * argv[])
 
     stor = qn_stor_create();
     if (!stor) {
+        qn_mac_destroy(mac);
         printf("Cannot initialize a new storage object.\n");
         return 1;
     } // if
@@ -33,9 +36,15 @@ int main(int argc, char * argv[])
     memset(&auth, 0, sizeof(auth));
     auth.server_end.mac = mac;
 
-    if (!qn_stor_fetch(stor, &auth, src_url, bucket, key, NULL)) {
+    fetch_ret = qn_stor_fetch(stor, &auth, src_url, bucket, key, NULL);
+    qn_mac_destroy(mac);
+    if (!fetch_ret) {
         printf("Cannot fetch the `%s` to the `%s:%s` file.\n", src_url, bucket, key);
         return 2;
+    } // if
+    if (qn_json_get_string(fetch_ret, "error", NULL)) {
+        printf("Cannot fetch the `%s` to the `%s:%s` file due to application error `%s`.\n", src_url, bucket, key, qn_json_get_string(fetch_ret, "error", NULL));
+        return 3;
     } // if
 
     hdr_itr = qn_stor_resp_get_header_iterator(stor);
@@ -44,16 +53,15 @@ int main(int argc, char * argv[])
     } // while
     qn_http_hdr_itr_destroy(hdr_itr);
 
-    fetch_ret = qn_json_object_to_string(qn_stor_get_object_body(stor));
-    if (!fetch_ret) {
-        printf("Cannot format the object body form /stat interface.\n");
+    fetch_ret_str = qn_json_object_to_string(fetch_ret);
+    qn_stor_destroy(stor);
+    if (!fetch_ret_str) {
+        printf("Cannot format the object body returned from the fetch interface.\n");
         return 3;
     } // if
 
-    printf("%s\n", fetch_ret);
-    qn_str_destroy(fetch_ret);
+    printf("%s\n", fetch_ret_str);
+    qn_str_destroy(fetch_ret_str);
 
-    qn_stor_destroy(stor);
-    qn_mac_destroy(mac);
     return 0;
 }
