f3483c9c501fa6033765384a47599f44c2eb6a52
LIANG Tao <amethyst.black@gmail.com>
2017-03-14 14:44:31 +0800

    更新版本号和 ChangeLog 。

--- src/qiniu/version.c
+++ src/qiniu/version.c
@@ -8,7 +8,7 @@ extern "C"
 {
 #endif
 
-static const char * qn_ver_full_string = "libqiniu-0.8.0";
+static const char * qn_ver_full_string = "libqiniu-0.9.0";
 
 QN_SDK const char * qn_ver_get_full_string(void)
 {
