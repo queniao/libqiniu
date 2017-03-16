d8de26dd2406200e354e2e28063d99face4ea6c1
LIANG Tao <amethyst.black@gmail.com>
2017-03-16 16:04:23 +0800

    为 User-Defined Variable 列表定义自有类型，避免误用。

--- src/qiniu/ud/variable.c
+++ src/qiniu/ud/variable.c
@@ -1,12 +1,16 @@
 #include <assert.h>
+
 #include "qiniu/ud/variable.h"
+#include "qiniu/base/errors.h"
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-QN_SDK qn_bool qn_ud_var_set_raw(qn_etable_ptr restrict ud, const char * restrict key, qn_size key_size, const char * restrict val, qn_size val_size)
+// ---- User-Defined Variable Functions (abbreviation: ud_var) ----
+
+QN_SDK qn_bool qn_ud_var_set_raw(qn_ud_variable_ptr restrict udv, const char * restrict key, qn_size key_size, const char * restrict val, qn_size val_size)
 {
     qn_bool ret = qn_false;
     qn_string tmp_key = NULL;
@@ -17,7 +21,7 @@ QN_SDK qn_bool qn_ud_var_set_raw(qn_etable_ptr restrict ud, const char * restric
     if (2 <= key_size) {
         // Case 1: x:name
         // Case 2: x:
-        if (posix_strncmp("x:", key, 2) == 0) return qn_etbl_set_raw(key, key_size, val, val_size);
+        if (posix_strncmp("x:", key, 2) == 0) return qn_etbl_set_raw((qn_etable_ptr) udv, key, key_size, val, val_size);
 
         if (posix_strncmp("X:", key, 2) == 0) {
             // Case 3: X:age
@@ -37,7 +41,7 @@ QN_SDK qn_bool qn_ud_var_set_raw(qn_etable_ptr restrict ud, const char * restric
     } // if
     if (! tmp_key) return qn_false;
 
-    ret = qn_etbl_set_raw(qn_str_cstr(tmp_key), qn_str_size(tmp_key), val, val_size);
+    ret = qn_etbl_set_raw((qn_etable_ptr) udv, qn_str_cstr(tmp_key), qn_str_size(tmp_key), val, val_size);
     qn_str_destroy(tmp_key);
     return ret;
 }
--- src/qiniu/ud/variable.h
+++ src/qiniu/ud/variable.h
@@ -10,68 +10,74 @@ extern "C"
 
 // ---- User-Defined Variable Functions (abbreviation: ud_var) ----
 
-static inline qn_etable_ptr qn_ud_var_create(void)
+struct _QN_UD_VARIABLE;
+typedef struct _QN_UD_VARIABLE * qn_ud_variable_ptr;
+
+static inline qn_ud_variable_ptr qn_ud_var_create(void)
 {
-    return qn_etbl_create("/");
+    return (qn_ud_variable_ptr) qn_etbl_create("/");
 }
 
-static inline void qn_ud_var_destroy(qn_etable_ptr restrict ud)
+static inline void qn_ud_var_destroy(qn_ud_variable_ptr restrict udv)
 {
-    qn_etbl_destroy(ud);
+    qn_etbl_destroy((qn_etable_ptr) udv);
 }
 
-static inline void qn_ud_var_reset(qn_etable_ptr restrict ud)
+static inline void qn_ud_var_reset(qn_ud_variable_ptr restrict udv)
 {
-    qn_etbl_reset(ud);
+    qn_etbl_reset((qn_etable_ptr) udv);
 }
 
-static inline int qn_ud_var_count(qn_etable_ptr restrict ud)
+static inline int qn_ud_var_count(qn_ud_variable_ptr restrict udv)
 {
-    return qn_etbl_count(ud);
+    return qn_etbl_count((qn_etable_ptr) udv);
 }
 
-QN_SDK extern qn_bool qn_ud_var_set_raw(qn_etable_ptr restrict ud, const char * restrict key, qn_size key_size, const char * restrict val, qn_size val_size);
+QN_SDK extern qn_bool qn_ud_var_set_raw(qn_ud_variable_ptr restrict udv, const char * restrict key, qn_size key_size, const char * restrict val, qn_size val_size);
 
-static inline qn_bool qn_ud_var_set_text(qn_etable_ptr restrict ud, const char * restrict key, const char * restrict val, qn_size val_size)
+static inline qn_bool qn_ud_var_set_text(qn_ud_variable_ptr restrict udv, const char * restrict key, const char * restrict val, qn_size val_size)
 {
-    return qn_ud_var_set_raw(ud, key, posix_strlen(key), val, val_size);
+    return qn_ud_var_set_raw(udv, key, posix_strlen(key), val, val_size);
 }
 
-static inline qn_bool qn_ud_var_set_string(qn_etable_ptr restrict ud, const char * restrict key, const char * restrict val)
+static inline qn_bool qn_ud_var_set_string(qn_ud_variable_ptr restrict udv, const char * restrict key, const char * restrict val)
 {
-    return qn_ud_var_set_raw(ud, key, posix_strlen(key), val, posix_strlen(val));
+    return qn_ud_var_set_raw(udv, key, posix_strlen(key), val, posix_strlen(val));
 }
 
-static inline void qn_ud_var_unset(qn_etable_ptr restrict ud, const char * restrict key)
+static inline void qn_ud_var_unset(qn_ud_variable_ptr restrict udv, const char * restrict key)
 {
-    qn_etbl_unset(ud, key);
+    qn_etbl_unset((qn_etable_ptr) udv, key);
 }
 
 // ---- User-Defined Variable Iterator Functions (abbreviation: ud_var_itr) ----
 
-static inline qn_etbl_iterator_ptr qn_ud_var_itr_create(qn_etable_ptr restrict ud)
+struct _QN_UD_VAR_ITERATOR;
+typedef struct _QN_UD_VAR_ITERATOR * qn_ud_var_iterator_ptr;
+
+static inline qn_ud_var_iterator_ptr qn_ud_var_itr_create(qn_ud_variable_ptr restrict udv)
 {
-    return qn_etbl_itr_create(ud);
+    return (qn_ud_var_iterator_ptr) qn_etbl_itr_create((qn_etable_ptr) udv);
 }
 
-static inline void qn_ud_var_itr_destroy(qn_etbl_iterator_ptr restrict itr)
+static inline void qn_ud_var_itr_destroy(qn_ud_var_iterator_ptr restrict itr)
 {
-    qn_etbl_itr_destroy(itr);
+    qn_etbl_itr_destroy((qn_etbl_iterator_ptr) itr);
 }
 
-static inline void qn_ud_var_itr_rewind(qn_etbl_iterator_ptr restrict itr)
+static inline void qn_ud_var_itr_rewind(qn_ud_var_iterator_ptr restrict itr)
 {
-    qn_etbl_itr_rewind(itr);
+    qn_etbl_itr_rewind((qn_etbl_iterator_ptr) itr);
 }
 
-static inline qn_string qn_ud_var_itr_next_entry(qn_etbl_iterator_ptr restrict itr)
+static inline qn_string qn_ud_var_itr_next_entry(qn_ud_var_iterator_ptr restrict itr)
 {
-    return qn_etbl_itr_next_entry(itr);
+    return qn_etbl_itr_next_entry((qn_etbl_iterator_ptr) itr);
 }
 
-static inline qn_bool qn_etbl_itr_next_pair_raw(qn_etbl_iterator_ptr restrict itr, const char ** restrict key, qn_size * restrict key_size, const char ** restrict val, qn_size * restrict val_size)
+static inline qn_bool qn_ud_var_itr_next_pair_raw(qn_ud_var_iterator_ptr restrict itr, const char ** restrict key, qn_size * restrict key_size, const char ** restrict val, qn_size * restrict val_size)
 {
-    return qn_etbl_itr_next_pair_raw(itr, key, key_size, val, val_size);
+    return qn_etbl_itr_next_pair_raw((qn_etbl_iterator_ptr) itr, key, key_size, val, val_size);
 }
 
 #ifdef __cplusplus
