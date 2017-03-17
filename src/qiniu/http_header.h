cb8e1a4e419a129218b53193661864cc592ceedf
LIANG Tao <amethyst.black@gmail.com>
2017-03-17 16:56:40 +0800

    将 HTTP Query 模块的函数内联化，减少不必要的 QN_SDK 函数符号。

--- src/qiniu/http_header.h
+++ src/qiniu/http_header.h
@@ -2,6 +2,7 @@
 #define __QN_HTTP_HEADER_H__
 
 #include "qiniu/base/string.h"
+#include "qiniu/base/ds/etable.h"
 #include "qiniu/macros.h"
 
 #ifdef __cplusplus
@@ -9,21 +10,45 @@ extern "C"
 {
 #endif
 
-// ---- Declaration of HTTP header ----
+// ---- HTTP Header Functions (abbreviation: http_hdr) ----
 
 struct _QN_HTTP_HEADER;
 typedef struct _QN_HTTP_HEADER * qn_http_header_ptr;
 
-QN_SDK extern qn_http_header_ptr qn_http_hdr_create(void);
-QN_SDK extern void qn_http_hdr_destroy(qn_http_header_ptr restrict hdr);
-QN_SDK extern void qn_http_hdr_reset(qn_http_header_ptr restrict hdr);
+static inline qn_http_header_ptr qn_http_hdr_create(void)
+{
+    return (qn_http_header_ptr) qn_etbl_create(": ");
+}
 
-QN_SDK extern int qn_http_hdr_count(qn_http_header_ptr restrict hdr);
+static inline void qn_http_hdr_destroy(qn_http_header_ptr restrict hdr)
+{
+    qn_etbl_destroy((qn_etable_ptr) hdr);
+}
 
-QN_SDK extern qn_string qn_http_hdr_get_entry(qn_http_header_ptr restrict hdr, const char * restrict key);
-QN_SDK extern const char * qn_http_hdr_get_value(qn_http_header_ptr restrict hdr, const char * restrict key);
+static inline void qn_http_hdr_reset(qn_http_header_ptr restrict hdr)
+{
+    qn_etbl_reset((qn_etable_ptr) hdr);
+}
 
-QN_SDK extern qn_bool qn_http_hdr_set_raw(qn_http_header_ptr restrict hdr, const char * restrict key, qn_size key_size, const char * restrict val, qn_size val_size);
+static inline int qn_http_hdr_count(qn_http_header_ptr restrict hdr)
+{
+    return qn_etbl_count((qn_etable_ptr) hdr);
+}
+
+static inline qn_string qn_http_hdr_get_entry(qn_http_header_ptr restrict hdr, const char * restrict key)
+{
+    return qn_etbl_get_entry((qn_etable_ptr) hdr, key);
+}
+
+static inline const char * qn_http_hdr_get_value(qn_http_header_ptr restrict hdr, const char * restrict key)
+{
+    return qn_etbl_get_value((qn_etable_ptr) hdr, key);
+}
+
+static inline qn_bool qn_http_hdr_set_raw(qn_http_header_ptr restrict hdr, const char * restrict key, qn_size key_size, const char * restrict val, qn_size val_size)
+{
+    return qn_etbl_set_raw((qn_etable_ptr) hdr, key, key_size, val, val_size);
+}
 
 static inline qn_bool qn_http_hdr_set_text(qn_http_header_ptr restrict hdr, const char * restrict key, const char * restrict val, qn_size val_size)
 {
@@ -35,19 +60,45 @@ static inline qn_bool qn_http_hdr_set_string(qn_http_header_ptr restrict hdr, co
     return qn_http_hdr_set_raw(hdr, key, strlen(key), val, strlen(val));
 }
 
-QN_SDK extern qn_bool qn_http_hdr_set_integer(qn_http_header_ptr restrict hdr, const char * restrict key, int val);
-QN_SDK extern void qn_http_hdr_unset(qn_http_header_ptr restrict hdr, const char * restrict key);
+static inline qn_bool qn_http_hdr_set_integer(qn_http_header_ptr restrict hdr, const char * restrict key, int val)
+{
+    return qn_etbl_set_integer((qn_etable_ptr) hdr, key, val);
+}
 
-// ---- Declaration of HTTP header iterator ----
+static inline void qn_http_hdr_unset(qn_http_header_ptr restrict hdr, const char * restrict key)
+{
+    qn_etbl_unset((qn_etable_ptr) hdr, key);
+}
 
-typedef void * qn_http_hdr_iterator_ptr;
+// ---- HTTP Header Iterator Functions (abbreviation: http_hdr_itr) ----
 
-QN_SDK extern qn_http_hdr_iterator_ptr qn_http_hdr_itr_create(qn_http_header_ptr restrict hdr);
-QN_SDK extern void qn_http_hdr_itr_destroy(qn_http_hdr_iterator_ptr restrict itr);
-QN_SDK extern void qn_http_hdr_itr_rewind(qn_http_hdr_iterator_ptr restrict itr);
+struct _QN_HTTP_HDR_ITERATOR;
+typedef struct _QN_HTTP_HDR_ITERATOR * qn_http_hdr_iterator_ptr;
 
-QN_SDK extern const qn_string qn_http_hdr_itr_next_entry(qn_http_hdr_iterator_ptr restrict itr);
-QN_SDK extern qn_bool qn_http_hdr_itr_next_pair_raw(qn_http_hdr_iterator_ptr restrict itr, const char ** restrict key, qn_size * restrict key_size, const char ** restrict val, qn_size * restrict val_size);
+static inline qn_http_hdr_iterator_ptr qn_http_hdr_itr_create(qn_http_header_ptr restrict hdr)
+{
+    return (qn_http_hdr_iterator_ptr) qn_etbl_itr_create((qn_etable_ptr) hdr);
+}
+
+static inline void qn_http_hdr_itr_destroy(qn_http_hdr_iterator_ptr restrict itr)
+{
+    qn_etbl_itr_destroy((qn_etbl_iterator_ptr) itr);
+}
+
+static inline void qn_http_hdr_itr_rewind(qn_http_hdr_iterator_ptr restrict itr)
+{
+    qn_etbl_itr_rewind((qn_etbl_iterator_ptr) itr);
+}
+
+static inline const qn_string qn_http_hdr_itr_next_entry(qn_http_hdr_iterator_ptr restrict itr)
+{
+    return qn_etbl_itr_next_entry((qn_etbl_iterator_ptr) itr);
+}
+
+static inline qn_bool qn_http_hdr_itr_next_pair_raw(qn_http_hdr_iterator_ptr restrict itr, const char ** restrict key, qn_size * restrict key_size, const char ** restrict val, qn_size * restrict val_size)
+{
+    return qn_etbl_itr_next_pair_raw((qn_etbl_iterator_ptr) itr, key, key_size, val, val_size);
+}
 
 #ifdef __cplusplus
 }
--- src/qiniu/http_query.h
+++ src/qiniu/http_query.h
@@ -5,23 +5,44 @@
 #include "qiniu/base/string.h"
 #include "qiniu/macros.h"
 
-// ---- Declaration of HTTP query ----
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+// ---- HTTP Header Functions (abbreviation: http_hdr) ----
+
 struct _QN_HTTP_QUERY;
 typedef struct _QN_HTTP_QUERY * qn_http_query_ptr;
 
-QN_SDK extern qn_http_query_ptr qn_http_qry_create(void);
-QN_SDK extern void qn_http_qry_destroy(qn_http_query_ptr restrict qry);
-QN_SDK extern void qn_http_qry_reset(qn_http_query_ptr restrict qry);
+static inline qn_http_query_ptr qn_http_qry_create(void)
+{
+    return (qn_http_query_ptr) qn_etbl_create("=");
+}
 
-QN_SDK extern int qn_http_qry_count(qn_http_query_ptr restrict qry);
+static inline void qn_http_qry_destroy(qn_http_query_ptr restrict qry)
+{
+    qn_etbl_destroy((qn_etable_ptr) qry);
+}
 
-QN_SDK extern qn_bool qn_http_qry_set_raw(qn_http_query_ptr restrict qry, const char * restrict key, qn_size key_size, const char * restrict val, qn_size val_size);
+static inline void qn_http_qry_reset(qn_http_query_ptr restrict qry)
+{
+    qn_etbl_reset((qn_etable_ptr) qry); 
+}
+
+static inline int qn_http_qry_count(qn_http_query_ptr restrict qry)
+{
+    return qn_etbl_count((qn_etable_ptr) qry);
+}
+
+static inline qn_bool qn_http_qry_set_raw(qn_http_query_ptr restrict qry, const char * restrict key, qn_size key_size, const char * restrict val, qn_size val_size)
+{
+    qn_bool ret;
+    qn_string encoded_val = qn_cs_percent_encode(val, val_size);
+    ret = qn_etbl_set_raw((qn_etable_ptr) qry, key, key_size, qn_str_cstr(encoded_val), qn_str_size(encoded_val));
+    qn_str_destroy(encoded_val);
+    return ret;
+}
 
 static inline qn_bool qn_http_qry_set_text(qn_http_query_ptr restrict qry, const char * restrict key, const char * restrict val, qn_size val_size)
 {
@@ -33,9 +54,45 @@ static inline qn_bool qn_http_qry_set_string(qn_http_query_ptr restrict qry, con
     return qn_http_qry_set_raw(qry, key, strlen(key), val, strlen(val));
 }
 
-QN_SDK extern qn_bool qn_http_qry_set_integer(qn_http_query_ptr restrict qry, const char * restrict key, int value);
+static inline qn_bool qn_http_qry_set_integer(qn_http_query_ptr restrict qry, const char * restrict key, int value)
+{
+    return qn_etbl_set_integer((qn_etable_ptr) qry, key, value);
+}
 
-QN_SDK extern qn_string qn_http_qry_to_string(qn_http_query_ptr restrict qry);
+static inline qn_string qn_http_qry_to_string(qn_http_query_ptr restrict qry)
+{
+    return qn_str_join_list("&", qn_etbl_entries((qn_etable_ptr) qry), qn_etbl_count((qn_etable_ptr) qry));
+}
+
+// ---- HTTP Query Iterator Functions (abbreviation: http_hdr_itr) ----
+
+struct _QN_HTTP_QRY_ITERATOR;
+typedef struct _QN_HTTP_QRY_ITERATOR * qn_http_qry_iterator_ptr;
+
+static inline qn_http_qry_iterator_ptr qn_http_qry_itr_create(qn_http_header_ptr restrict qry)
+{
+    return (qn_http_qry_iterator_ptr) qn_etbl_itr_create((qn_etable_ptr) qry);
+}
+
+static inline void qn_http_qry_itr_destroy(qn_http_qry_iterator_ptr restrict itr)
+{
+    qn_etbl_itr_destroy((qn_etbl_iterator_ptr) itr);
+}
+
+static inline void qn_http_qry_itr_rewind(qn_http_qry_iterator_ptr restrict itr)
+{
+    qn_etbl_itr_rewind((qn_etbl_iterator_ptr) itr);
+}
+
+static inline const qn_string qn_http_qry_itr_next_entry(qn_http_qry_iterator_ptr restrict itr)
+{
+    return qn_etbl_itr_next_entry((qn_etbl_iterator_ptr) itr);
+}
+
+static inline qn_bool qn_http_qry_itr_next_pair_raw(qn_http_qry_iterator_ptr restrict itr, const char ** restrict key, qn_size * restrict key_size, const char ** restrict val, qn_size * restrict val_size)
+{
+    return qn_etbl_itr_next_pair_raw((qn_etbl_iterator_ptr) itr, key, key_size, val, val_size);
+}
 
 #ifdef __cplusplus
 }
