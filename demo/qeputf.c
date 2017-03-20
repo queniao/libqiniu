9e0b808948f6efa9410eeeee220dbfeb47fe68ac
LIANG Tao <amethyst.black@gmail.com>
2017-03-20 11:04:55 +0800

    修改 Easy 模块上传函数的测试用例，以设置上传策略字段和／或用户自定义变量。

--- demo/qeputf.c
+++ demo/qeputf.c
@@ -7,21 +7,26 @@
 
 int main(int argc, char * argv[])
 {
+    int i = 0;
     const char * bucket;
     const char * key;
     const char * fname;
+    const char * return_url = NULL;
+    const char * return_body = NULL;
+    const char * pos = NULL;
     qn_mac_ptr mac;
     qn_string local_qetag;
     qn_string uptoken;
     qn_string put_ret_str;
     qn_json_object_ptr put_policy;
     qn_json_object_ptr put_ret;
+    qn_ud_variable_ptr ud_vars = NULL;
     qn_easy_ptr easy;
     qn_easy_put_extra_ptr pe;
 
-    if (argc < 5) {
+    if (argc < 6) {
         printf("Demo qeputf - Put single file in an easy and smart way.\n");
-        printf("Usage: qeputf <ACCESS_KEY> <SECRET_KEY> <BUCKET> <KEY> <FNAME>\n");
+        printf("Usage: qeputf <ACCESS_KEY> <SECRET_KEY> <BUCKET> <KEY> <FNAME> [PUT_POLICY_FIELD=\"PUT_POLICY_VALUE\"]\n");
         return 0;
     } // if
 
@@ -42,6 +47,56 @@ int main(int argc, char * argv[])
         return 1;
     } // if
 
+    if (argc > 6) {
+        for (i = 6; i < argc; i += 1) {
+            if (strncmp(argv[i], "x:", 2) == 0) {
+                pos = strchr(argv[i], '=');
+                if (! pos) {
+                    printf("Unknown option: [%s], skipped.\n", argv[i]);
+                    continue;
+                } // if
+
+                if (! ud_vars && ! (ud_vars = qn_ud_var_create())) {
+                    printf("Cannot create a user-defined variable table due to application error `%s`\n", qn_err_get_message());
+                    qn_json_destroy_object(put_policy);
+                    exit(1);
+                } // if
+
+                if (! qn_ud_var_set_raw(ud_vars, argv[i], pos - argv[i], pos + 1, strlen(pos + 1))) {
+                    printf("Cannot set a user-defined variable table due to application error `%s`\n", qn_err_get_message());
+                    qn_json_destroy_object(put_policy);
+                    exit(1);
+                } // if
+            } else {
+                pos = strchr(argv[i], '=');
+                if (! pos) {
+                    printf("Unknown option: [%s], skipped.\n", argv[i]);
+                    continue;
+                } // if
+
+                if (strncmp(argv[i], "RETURN_URL", 10) == 0) {
+                    return_url = pos + 1;
+                } else if (strncmp(argv[i], "RETURN_BODY", 11) == 0) {
+                    return_body = pos + 1;
+                } // if
+            } // if
+        } // for
+
+        if (return_url) {
+            if (! qn_stor_pp_return_to_server(put_policy, return_url, return_body)) {
+                printf("Cannot set return url and body due to application error `%s`\n", qn_err_get_message());
+                qn_json_destroy_object(put_policy);
+                exit(1);
+            } // if
+        } else if (return_body) {
+            if (! qn_stor_pp_return_to_client(put_policy, return_body)) {
+                printf("Cannot set return url and body due to application error `%s`\n", qn_err_get_message());
+                qn_json_destroy_object(put_policy);
+                exit(1);
+            } // if
+        } // if
+    } // if
+
     uptoken = qn_stor_pp_to_uptoken(put_policy, mac);
     qn_json_destroy_object(put_policy);
     qn_mac_destroy(mac);
@@ -59,6 +114,8 @@ int main(int argc, char * argv[])
 
     qn_easy_pe_set_final_key(pe, key);
     qn_easy_pe_set_qetag_checking(pe, qn_true);
+
+    qn_easy_pe_set_user_defined_variables(pe, ud_vars);
 
     easy = qn_easy_create();
     if (! easy) {
